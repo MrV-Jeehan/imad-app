2597aa8561f43b8ebdb48a13194a998fe4bf2b58
MrV-Jeehan <vikas.agarwal1081@gmail.com>
2017-08-29 22:25:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,9 +9,9 @@ element.innerHTML = "New text";
 var img = document.getElementById('abc');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft +10;
+    marginLeft = marginLeft +1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight,10);
 };
