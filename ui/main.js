d8de825669f5ace6d189d212a05db4cbc24643c6
MrV-Jeehan <vikas.agarwal1081@gmail.com>
2017-08-29 22:13:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-txt');
 element.innerHTML = "New text";
+
+//Move image
+
+var img = document.getElementById('abc');
+img.onclick = function () {
+    img.style.marginLeft = '100px';  
+};
