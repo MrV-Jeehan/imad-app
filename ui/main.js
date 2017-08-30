a23052f4c51f07abc8a171ca957802421b337aef
MrV-Jeehan <vikas.agarwal1081@gmail.com>
2017-08-30 22:56:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,21 @@
 //Counter
 var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function (){
+
+button.onclick = function () {
    //Make Request
-   
+   var request = new XMLHttpRequest();
    //Capture and store
+   request.onreadystatechange() = function () {
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }  
+   };
    
-   //Render Variable in span
-   counter = counter +1;
-   var span = document.getElementById('count');
-   span.innerHTML = counter.toString();
+   request.open('GET',"http://http://vikas71845.imad.hasura-app.io/counter",true);
+   request.send(null); 
+   
 };
