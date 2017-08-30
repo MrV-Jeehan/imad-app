7fc55ea47c09661bffeff8a7a10ade09fbd094fd
MrV-Jeehan <vikas.agarwal1081@gmail.com>
2017-08-30 22:45:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,13 @@
-console.log('Loaded!');
-//Change text
-
-//Move image
-
-var img = document.getElementById('abc');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft +1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight,50);
+//Counter
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function (){
+   //Make Request
+   
+   //Capture and store
+   
+   //Render Variable in span
+   counter = counter +1;
+   var span = document.getElementById('count');
+   span.innerHTML = counter.toString();
 };
