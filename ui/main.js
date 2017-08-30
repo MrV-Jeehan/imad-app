acecb4a26695ad446239f9c819708ae7ebfb4b12
MrV-Jeehan <vikas.agarwal1081@gmail.com>
2017-08-30 22:29:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,6 @@
 console.log('Loaded!');
 //Change text
 
-var element = document.getElementById('main-txt');
-element.innerHTML = "New text";
-
 //Move image
 
 var img = document.getElementById('abc');
