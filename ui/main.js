5ad518a7505fc4866477db18bf5fe78995c5bf71
MrV-Jeehan <vikas.agarwal1081@gmail.com>
2017-08-31 10:45:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,12 +24,24 @@ var name = nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick = function(){
     //Make Request
+     var request = new XMLHttpRequest();
+   //Capture and store
+   request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }  
+   };
+   
+   request.open('GET',"http://vikas71845.imad.hasura-app.io/submit-name?name="+name,true);
+   request.send(null); 
     //Render Response
-    var names = ['name1','name2','name3'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
 };
